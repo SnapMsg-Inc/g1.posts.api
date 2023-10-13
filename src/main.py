86a1248d42ed5c2fb796dd4271d6f9ec557e8e4d
marioax <santiagoadris171@gmail.com>
2023-10-13 14:40:47 -0300

    change in file structure

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-import psycopg2
-
 
 app = FastAPI()
 
@@ -8,13 +6,6 @@ app = FastAPI()
 async def root():
 	return {"message": "Posts microsevice"}
 
-con = psycopg2.connect(
-	database="postsdb",
-	user="snapmsg",
-	password="1234",
-	host="posts-db",
-	port= '5432'
-)
 #
 #    posts routes
 #
